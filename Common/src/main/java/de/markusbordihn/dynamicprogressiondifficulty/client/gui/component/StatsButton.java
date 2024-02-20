7df1aed7b78b40613acda798788b482f3c2af1a4
Markus Bordihn <Markus@Bordihn.de>
2024-02-20 23:09:37 +0100

    Fixed Fabric port dependencies. Added additional item class icons. Added dealt and hurt damage adjustments to the player stats screen. Improved item tooltip manager.

---
+++ Common/src/main/java/de/markusbordihn/dynamicprogressiondifficulty/client/gui/component/StatsButton.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2022 Markus Bordihn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package de.markusbordihn.dynamicprogressiondifficulty.client.gui.component;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import de.markusbordihn.dynamicprogressiondifficulty.Constants;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+
+public class StatsButton extends Button {
+
+  public static final int OFFSET_X = 30;
+  public static final int OFFSET_Y = 26;
+  private static final ResourceLocation TEXTURE =
+      new ResourceLocation(Constants.MOD_ID, "textures/gui/texture.png");
+  private static final int WIDTH = 20;
+  private static final int HEIGHT = 20;
+
+  public StatsButton(int x, int y, OnPress onPress) {
+    super(x, y, WIDTH, HEIGHT, Component.literal(""), onPress);
+  }
+
+  public static int getOffsetX() {
+    return OFFSET_X;
+  }
+
+  public static int getOffsetY() {
+    return OFFSET_Y;
+  }
+
+  @Override
+  public void renderButton(PoseStack poseStack, int mouseX, int mouseY, float partialTicks) {
+    super.renderButton(poseStack, mouseX, mouseY, partialTicks);
+    RenderSystem.setShader(GameRenderer::getPositionTexShader);
+    RenderSystem.setShaderTexture(0, TEXTURE);
+    if (isHoveredOrFocused()) {
+      blit(poseStack, x + 6, y + 5, 34, 1, 10, 10);
+      this.renderToolTip(poseStack, mouseX, mouseY);
+    } else {
+      blit(poseStack, x + 6, y + 5, 24, 1, 10, 10);
+    }
+  }
+}
---
+++ Common/src/main/java/de/markusbordihn/dynamicprogressiondifficulty/network/message/UpdatePlayerStatsMessage.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2022 Markus Bordihn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package de.markusbordihn.dynamicprogressiondifficulty.network.message;
+
+import de.markusbordihn.dynamicprogressiondifficulty.Constants;
+import io.netty.buffer.Unpooled;
+import java.util.UUID;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+
+public class UpdatePlayerStatsMessage {
+
+  public static final ResourceLocation MESSAGE_ID =
+      new ResourceLocation(Constants.MOD_ID, "update_player_stats_message");
+
+  private final UUID uuid;
+
+  public UpdatePlayerStatsMessage(UUID uuid) {
+    this.uuid = uuid;
+  }
+
+  public static UpdatePlayerStatsMessage decode(final FriendlyByteBuf buffer) {
+    return new UpdatePlayerStatsMessage(buffer.readUUID());
+  }
+
+  public static FriendlyByteBuf encode(
+      final UpdatePlayerStatsMessage message, final FriendlyByteBuf buffer) {
+    buffer.writeUUID(message.uuid);
+    return buffer;
+  }
+
+  public FriendlyByteBuf encode() {
+    return encode(this, new FriendlyByteBuf(Unpooled.buffer()));
+  }
+}
